191fae9a97e840d773b6ca8f158707fcacdb2ce1
Richard Hood <rick@flowmediadesign.com>
2017-02-19 08:18:24 -0500

    added _settings.local.php

---
+++ web/sites/default/_settings.local.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all envrionments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+// include __DIR__ . "/settings.pantheon.php";
+
+/**
+ * Place the config directory outside of the Drupal root.
+ */
+$config_directories = array(
+  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
+);
+
+
+/**
+ * Always install the 'standard' profile to stop the installer from
+ * modifying settings.php.
+ *
+ * See: tests/installer-features/installer.feature
+ */
+$settings['install_profile'] = 'standard';
+$databases['default']['default'] = array (
+  'database' => 'resistance',
+  'username' => 'root',
+  'password' => 'password',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = '3NZ0NBxGZu_-9lPqIqvnomTiIqtpFRNQK30ZeFMcBSO77J_FnfTwX62GE4zof_J9e9ba9K3n1w';
+
